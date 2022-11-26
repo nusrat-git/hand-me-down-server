68681c150605b4c38902c169e175fe66ba58f983
nusrat-git <nusratjahanmithila66@gmail.com>
2022-11-26 20:30:20 +0600

    admin and seller route created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const e = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -15,6 +17,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.get('/', (req, res) => {
   res.send('Hand Me Down server running')
 })
+
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send('Unauthorized access');
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.Access_token, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 async function run() {
   try {
@@ -89,6 +109,60 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const result = await usersDatabase.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get('/users/seller/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersDatabase.findOne(query);
+      console.log({ isSeller: user?.role === 'Seller' });
+      res.send({ isSeller: user?.role === 'Seller' });
+    })
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersDatabase.findOne(query);
+      console.log({ isAdmin: user?.role === 'Admin' });
+      res.send({ isAdmin: user?.role === 'Admin' });
+    })
+
+
+    app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersDatabase.findOne(query);
+      if (user.role !== 'Admin') {
+        return res.status(403).send('forbidden access');
+      }
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          role: 'Admin'
+        }
+      }
+      const result = await usersDatabase.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    })
+
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersDatabase.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.Access_token, { expiresIn: '8h' })
+        return res.send({ accessToken: token });
+      }
+      console.log(user);
+      res.status(403).send({ accessToken: '' });
+    })
+
     app.post('/booked', async (req, res) => {
       const product = req.body;
       const result = await bookedDatabase.insertOne(product);
@@ -96,13 +170,26 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/booked', async (req, res) => {
-      const query = {};
-      const cursor = bookedDatabase.find(query);
-      const result = await cursor.toArray();
+    // app.get('/booked', verifyJWT, async (req, res) => {
+    //   const query = {};
+    //   const decodedEmail = req.decoded.email;
+    //   const cursor = bookedDatabase.find(query);
+    //   const result = await cursor.toArray();
+    //   res.send(result);
+    // })
+
+    app.get('/booked', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+
+      const query = { buyer_email: email };
+      const result = await bookedDatabase.find(query).toArray();
       res.send(result);
     })
-
 
   }
   finally {
